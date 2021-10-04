56c507e2b3c4d74d7371b216b43145b2dc14aef7
Mabel Christina <maybelchristina@gmail.com>
2021-10-04 20:42:26 +0530

    [Mabel] Add:UC2 added

---
+++ DataStructuresDemo/LinkedListStack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresDemo
+{
+    class LinkedListStack
+    {
+        Node top;
+        public LinkedListStack()
+        {
+            this.top = null;
+        }
+        public void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed at stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("LL is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        public void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack", this.top.data);
+        }
+        public void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion not possible");
+                return;
+            }
+            Console.WriteLine("value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+    }
+}
--- DataStructuresDemo/Program.cs
+++ DataStructuresDemo/Program.cs
@@ -6,26 +6,34 @@ namespace DataStructuresDemo
     {
         static void Main(string[] args)
         {
-            LinkedListDemo linkedListDemo = new LinkedListDemo();
-            linkedListDemo.Add(56);
-            linkedListDemo.Add(30);
-            linkedListDemo.Add(70);
-            linkedListDemo.Display();
-            ////linkedListDemo.AddInReverseOrder(70);
-            ////linkedListDemo.AddInReverseOrder(30);
-            ////linkedListDemo.AddInReverseOrder(56);
-            //linkedListDemo.InsertAtParticularPosition(1, 30);
-            //Console.WriteLine("After inserting at a particular position");
+            //LinkedListDemo linkedListDemo = new LinkedListDemo();
+            //linkedListDemo.Add(56);
+            //linkedListDemo.Add(30);
+            //linkedListDemo.Add(70);
             //linkedListDemo.Display();
-            //linkedListDemo.RemoveFirstNode();
-            //Console.WriteLine("After deleting firt node");
+            //////linkedListDemo.AddInReverseOrder(70);
+            //////linkedListDemo.AddInReverseOrder(30);
+            //////linkedListDemo.AddInReverseOrder(56);
+            ////linkedListDemo.InsertAtParticularPosition(1, 30);
+            ////Console.WriteLine("After inserting at a particular position");
+            ////linkedListDemo.Display();
+            ////linkedListDemo.RemoveFirstNode();
+            ////Console.WriteLine("After deleting firt node");
+            ////linkedListDemo.Display();
+            //linkedListDemo.RemoveLastNode();
+            //Console.WriteLine("After deleting last node");
             //linkedListDemo.Display();
-            linkedListDemo.RemoveLastNode();
-            Console.WriteLine("After deleting last node");
-            linkedListDemo.Display();
-            Console.WriteLine("Searching");
-            linkedListDemo.Search(30);
-            linkedListDemo.Display();
+            //Console.WriteLine("Searching");
+            //linkedListDemo.Search(30);
+            //linkedListDemo.Display();
+
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(56);
+            linkedListStack.push(30);
+            linkedListStack.push(70);
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.Display();
         }
     }
 }
